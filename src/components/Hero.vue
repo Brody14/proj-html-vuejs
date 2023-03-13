652d86c39267d0ef3ab57c0bf959a78634c1b94d
Brody14 <stefania.spinas@gmail.com>
2023-03-13 15:49:31 +0100

    FEAT hero section

---
+++ src/components/Hero.vue
@@ -0,0 +1,59 @@
+<script>
+</script>
+
+<template>
+    <section class="hero">
+        <div class="container">
+            <div class="hero-text">
+                <h1 class="hero-text__title">
+                    Start your Human <span class="title-yellow">Revolution</span> today
+                </h1>
+                <p class="hero-text__paragraph">A wonderful serenity has taken possession on my entire soul, like these
+                    sweet
+                    mornings of spring which I
+                    enjoy with my whole heart.</p>
+
+            </div>
+        </div>
+
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+
+.hero {
+    height: 820px;
+    background-image: url("/images/slide-1.jpg");
+
+    .hero-text {
+
+        width: 730px;
+        height: 680px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        margin-left: auto;
+
+
+        .hero-text__title {
+            font-size: 70px;
+            font-family: 'Roboto slab', serif;
+            font-weight: 700;
+            line-height: 70px;
+            margin-bottom: 40px;
+
+            .title-yellow {
+                color: $yellow;
+            }
+        }
+
+        .hero-text__paragraph {
+            font-family: 'Open Sans', sans-serif;
+            font-size: 18px;
+            line-height: 35px;
+            width: 570px;
+        }
+    }
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<script>
+import Hero from './Hero.vue';
+
+export default {
+    components: {
+        Hero,
+    }
+}
+</script>
+
+<template>
+    <main>
+        <Hero />
+
+    </main>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+</style>
